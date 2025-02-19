25fe165de6e36e6ac745ea89966e38685b3cd265
hiiamcailyn <152474332+hiiamcailyn@users.noreply.github.com>
2025-02-19 18:09:36 +0000

    removed the end word

--- Main.java
+++ Main.java
@@ -8,9 +8,24 @@ public class Main
     Scanner sc = new Scanner(System.in);
     String input = "";
 
+    ArrayList<String> word = new ArrayList<String>();
+    
+    System.out.println("Please enter words, enter STOP to stop the loop.");
+
     while (!input.equals("STOP"))
     {
-      System.out.println("infinite loop! replace with your code");
+      System.out.println("infinite loop! enter a word");
+      input = sc.nextLine();
+      word.add(input);
+
+      System.out.println("Array list is now" + word);
     }
+    System.out.println("Left the loop. List is now" + word);
+    System.out.println("I will remove the last word");
+
+    int lastIndex = word.size()-1;
+    word.remove(lastIndex);
+
+    System.out.println("removed the last word. List is now" + word);
   }
 }
